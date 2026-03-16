2dd24a941854b38012b25876bd918c5d318cea82
sanjaykumarvaishya <sureshvaishya321@gmail.com>
2026-03-16 10:13:54 +0530

    set

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen pb-20 mx-auto`}>
+    <section className={`relative w-full h-screen mx-auto`}>
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -26,6 +26,24 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+
+      <div className='absolute xs:bottom-32 bottom-10 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
